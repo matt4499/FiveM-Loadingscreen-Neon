f91f6b6a784eea3e96f77556cca3f110d0aa254e
Atlass_75 <p.atlass75@gmail.com>
2018-11-04 00:56:17 +0100

    Update config.js

--- Assets/JS/config.js
+++ Assets/JS/config.js
@@ -1,3 +1,2 @@
-Let Video_ID = ""
-
+let Video_ID = "YhtPloQeA0c" // L'ID de la vidéo YouTube
 $('.VDO').attr('src', 'https://www.youtube.com/embed/' + Video_ID + '?autoplay=1&mute=1&loop=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&controls=0&volume=0')
